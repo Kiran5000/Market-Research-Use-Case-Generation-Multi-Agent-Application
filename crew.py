6572e89775a8d8f4f58c5c2ce19c4b20570dd12c
Kiran5000 <88710822+Kiran5000@users.noreply.github.com>
2025-04-14 23:19:38 +0530

    Update crew.py

--- crew.py
+++ crew.py
@@ -1,13 +1,14 @@
-from crewai import Crew, Process
-from agents import industry_researcher, use_case_generator, resource_collector
-from tasks import industry_research_task, use_case_generation_task, resource_collection_task
+from crewai import Crew,Process
+from agents import industry_researcher,use_case_generator,resource_collector
+from tasks import industry_research_task,use_case_generation_task,resource_collection_task
 
-aI_crew = Crew(
+ai_use_case_crew = Crew(
     agents=[industry_researcher, use_case_generator, resource_collector],
     tasks=[industry_research_task, use_case_generation_task, resource_collection_task],
-    process=Process.sequential,
+    process=Process.sequential,  # or hierarchical, depending on our preference
+    
     verbose=True
 )
-
 def ask(question):
-    return aI_crew.kickoff(inputs={'company': question})
+    global ai_use_case_crew
+    return ai_use_case_crew.kickoff(inputs={'company':question})
