b0f561c17033ec2a98c0aa716585faea91391bb9
Kiran5000 <88710822+Kiran5000@users.noreply.github.com>
2025-04-14 23:38:33 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,33 +1,26 @@
-from crewai_tools import ScrapeWebsiteTool, SerperDevTool,PDFSearchTool
-import os 
-from dotenv import load_dotenv
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool, PDFSearchTool
+import streamlit as st
+import os
 
-load_dotenv()
+os.environ['SERPER_API_KEY'] = st.secrets["SERPER_API_KEY"]
+os.environ['GOOGLE_API_KEY'] = st.secrets["GOOGLE_API_KEY"]
 
-os.environ['SERPER_API_KEY']=os.getenv('SERPER_API_KEY')
-os.environ['GOOGLE_API_KEY']=os.getenv('GOOGLE_API_KEY')
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
 
 pdf_tool = PDFSearchTool(
-    
-    
     config=dict(
         llm=dict(
             provider="google",
-            config=dict(
-                model="gemini-1.5-flash-002",
-
-            ),
+            config=dict(model="gemini-1.5-flash-002"),
         ),
         embedder=dict(
-            provider="google", 
+            provider="google",
             config=dict(
                 model="models/embedding-001",
                 task_type="retrieval_document",
             ),
         ),
     ),
-    pdf='.\report.pdf')
-
-
-search_tool = SerperDevTool()
-scrape_tool = ScrapeWebsiteTool()
+    pdf='./report.pdf'
+)
